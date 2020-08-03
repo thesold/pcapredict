7da66b077e530bfac7bc31e15624d151d245b078
m2de <m2de@outlook.com>
2020-08-03 11:36:48 +0100

    Export CJS format to fix imports

--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,7 @@ export default [
     {
         input: 'src/index.ts',
         output: {
-            format: 'es',
+            format: 'cjs',
             dir: './dist'
         },
         external: [
